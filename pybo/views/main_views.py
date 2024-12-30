3e870a52a7ced7f584fbe5acc730b57056e3bbf2
j1mi <kingone7@naver.com>
2024-12-30 19:33:00 +0900

    modify main_views.py for logging

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for
+from flask import Blueprint, url_for, current_app
 from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
@@ -9,5 +9,6 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
+    current_app.logger.info("Output at INFO level")
     return redirect(url_for('question._list')) #question, _list 순서로 해석되어 라우팅 함수 파인딩
 
